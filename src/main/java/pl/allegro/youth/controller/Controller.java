dfc1b5669b4e136f1ca9afc908caeec445ee5cbd
allegroyouth <allegroyouth@gmail.com>
2015-05-08 18:01:19 +0200

    Klasa Hour

--- src/main/java/pl/allegro/youth/controller/Controller.java
+++ src/main/java/pl/allegro/youth/controller/Controller.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/")
-public class Controller
-{
+@RequestMapping("/")
+public class Controller {
     @RequestMapping("hello")
- public String hello (){
-     return "hello";
- }
+    public String hello() {
+        return "hello";
+    }
 }
---
+++ src/main/java/pl/allegro/youth/model/Hour.java
@@ -0,0 +1,33 @@
+package pl.allegro.youth.model;
+
+
+public class Hour {
+
+    private int number;
+    private int start;
+    private int end;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+}
