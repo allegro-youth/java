fa9487111b61a5498423570fe7d90653e4021f3d
allegroyouth <michal7721@o2.pl>
2015-05-08 18:03:33 +0200

    Class

---
+++ src/main/java/pl/allegro/youth/model/Class.java
@@ -0,0 +1,32 @@
+package pl.allegro.youth.model;
+
+
+public class Class {
+    private int id;
+    private String classname;
+    private String classinfo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getClassinfo() {
+        return classinfo;
+    }
+
+    public void setClassinfo(String classinfo) {
+        this.classinfo = classinfo;
+    }
+}
