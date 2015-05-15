e1097b4584a90da5cc3ffc8455239968576a71b8
allegroyouth <allegroyouth@gmail.com>
2015-05-15 18:06:07 +0200

    a

--- src/main/java/pl/allegro/youth/controller/Controller.java
+++ src/main/java/pl/allegro/youth/controller/Controller.java
@@ -3,6 +3,7 @@ package pl.allegro.youth.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.allegro.youth.model.ClassRoom;
+import pl.allegro.youth.model.Teacher;
 
 @RestController
 @RequestMapping("/")
@@ -12,11 +13,18 @@ public class Controller {
         return "hello";
     }
 
-
-
     @RequestMapping("classroom")
     public ClassRoom classRoom(){
         return new ClassRoom(1, "Budynek główny", 70);
+
+    }
+
+    @RequestMapping("teacher")
+    public String getTeachers(){
+        Teacher first = new Teacher(1, "Andrzej", "Gac");
+        Teacher secend = new Teacher(2, "Renata", "Gac");
+
+        return first.toString() + " " + secend.toString();
 
     }
 
--- src/main/java/pl/allegro/youth/model/Teacher.java
+++ src/main/java/pl/allegro/youth/model/Teacher.java
@@ -2,9 +2,68 @@ package pl.allegro.youth.model;
 
 public class Teacher {
 
+    private Integer id;
+    private String firstName;
+    private String lastName;
 
-    private String name;
-    
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Teacher(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Teacher teacher = (Teacher) o;
+
+        if (!firstName.equals(teacher.firstName)) return false;
+        if (!id.equals(teacher.id)) return false;
+        if (!lastName.equals(teacher.lastName)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
+    }
 }
