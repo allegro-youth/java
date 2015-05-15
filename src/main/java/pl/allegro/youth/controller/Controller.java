42c8c0b44be88ed917ad4256d12d4576e90c64c2
allegroyouth <allegroyouth@gmail.com>
2015-05-15 17:17:52 +0200

    Classromm endpoint

--- src/main/java/pl/allegro/youth/controller/Controller.java
+++ src/main/java/pl/allegro/youth/controller/Controller.java
@@ -2,6 +2,7 @@ package pl.allegro.youth.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.allegro.youth.model.ClassRoom;
 
 @RestController
 @RequestMapping("/")
@@ -10,4 +11,17 @@ public class Controller {
     public String hello() {
         return "hello";
     }
+
+
+
+    @RequestMapping("classroom")
+    public ClassRoom classRoom(){
+        ClassRoom classRoom = new ClassRoom();
+        classRoom.setId(1);
+        classRoom.setNr(70);
+        classRoom.setBuilding("Główny");
+
+        return classRoom;
+    }
+
 }
